cde41839c15ff3d83ad830e46982ae6fbf8b97e8
Sergey Tihon <sergey.tihon@gmail.com>
2015-12-09 14:58:37 +0300

    Initial commit

---
+++ src/SPUG.Newsletter/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace SPUG.Newsletter.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("SPUG.Newsletter")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("EPAM Systems")>]
+[<assembly: AssemblyProduct("SPUG.Newsletter")>]
+[<assembly: AssemblyCopyright("Copyright © EPAM Systems 2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("d55b4f4b-1eac-46f3-a02a-09a0127ad05b")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/SPUG.Newsletter/Program.fs
@@ -0,0 +1,139 @@
+﻿open System
+open System.Net
+open System.Text.RegularExpressions
+open System.Globalization
+open TweetSharp
+
+// Search configuration
+open System.Configuration
+
+#if INTERACTIVE // Values used
+let key       = "wk8ICcCgJY2iXa6WS0PDZiUYC"
+let secret    = "AwA9G4fnwfPciCANx6QuWBaj009dwvAHFx3UW1XQD6yadH88py"
+let queries   = [|"#Office365"|]
+let minRetweetCount = 2
+#else
+let key       = ConfigurationManager.AppSettings.["consumerKey"]
+let secret    = ConfigurationManager.AppSettings.["consumerSecret"]
+let queries   = ConfigurationManager.AppSettings.["Queries"].Split([|';'|])
+let minRetweetCount = Int32.Parse(ConfigurationManager.AppSettings.["MinRetweetCount"])
+#endif
+
+
+// ------------ Twitter authentication ---------------------
+
+// Pass your app credentials to the service
+let service = new TwitterService(key, secret)
+// Step 1 - Retrieve an OAuth Request Token
+let requestToken = service.GetRequestToken()
+// Step 2 - Redirect to the OAuth Authorization URL
+let uri = service.GetAuthorizationUri(requestToken)
+System.Diagnostics.Process.Start(uri.ToString()) |> ignore
+// Step 3 - Exchange the Request Token for an Access Token
+printf "Please, enter PIN (from browser window):"
+let verifier = Console.ReadLine()
+let access = service.GetAccessToken(requestToken, verifier)
+// Step 4 - User authenticates using the Access Token
+service.AuthenticateWith(access.Token, access.TokenSecret)
+
+
+// Export tweets by query
+let getTweets query =
+    let rec collect (maxId:Nullable<int64>) attempt =
+        let options = SearchOptions(Q = query, Count = Nullable(200),
+                                    Resulttype = Nullable(TwitterSearchResultType.Recent))
+        if (maxId.HasValue) then options.MaxId <- maxId
+        printfn "Loading %s under id %A" query maxId
+        let results =
+            match service.Search(options) with
+            | null -> []
+            | res ->  res.Statuses |> Seq.toList
+        printfn "\t Loaded %d tweets" results.Length
+        if (results.Length <= 1)
+        then
+            if attempt > 0
+            then
+                printfn "Magic sleep for 20sec to be sure that server does not have results..."
+                Threading.Thread.Sleep(TimeSpan.FromSeconds(20.0))
+                collect maxId (attempt-1)
+            else
+                []
+        else
+            let lastTweet = results |> Seq.minBy (fun x->x.Id)
+            if (not maxId.HasValue || lastTweet.Id < maxId.Value)
+                then results |> List.append (collect (Nullable(lastTweet.Id)) 1)
+                else results
+    collect (Nullable()) 1 |> List.rev
+
+let urlRegexp = Regex("((mailto\:|(news|(ht|f)tp(s?))\://){1}\S+)", RegexOptions.IgnoreCase)
+
+let filterUniqLinks (tweets: TwitterStatus list) =
+    let hash = new System.Collections.Generic.HashSet<string>();
+    tweets |> List.fold
+        (fun acc t ->
+            let mathces = urlRegexp.Matches(t.Text)
+            if (mathces.Count = 0) then acc
+            else let urls =
+                    [0 .. (mathces.Count-1)]
+                    |> List.map (fun i -> mathces.[i].Value)
+                    |> List.filter (hash.Contains >> not)
+                 if (List.isEmpty urls) then acc
+                 else urls |> List.iter(hash.Add >> ignore)
+                      t :: acc)
+        [] |> List.rev
+
+let allTweets =
+    List.ofArray queries
+    |> List.map getTweets
+    |> List.concat
+
+let filteredTweets =
+    allTweets
+    |> List.filter (fun t->  t.RetweetCount >= minRetweetCount)
+    |> List.sortBy (fun t -> t.CreatedDate)
+    |> filterUniqLinks
+
+printfn "All tweets count : %d" allTweets.Length
+printfn "Filtered tweets count : %d" filteredTweets.Length
+
+let printTweetsInHtml filename (tweets: TwitterStatus list) =
+    let formatTweet (text:string) =
+        let matches = urlRegexp.Matches(text)
+        seq {0 .. (matches.Count-1)}
+            |> Seq.fold (
+                fun (t:string) i ->
+                    let url = matches.[i].Value
+                    t.Replace(url, (sprintf "<a href=\"%s\" target=\"_blank\">%s</a>" url url)))
+                text
+    let rows =
+        tweets
+        |> List.mapi (fun i t ->
+            let id = (tweets.Length - i)
+            let text = formatTweet(t.Text)
+            let tweetUrl = sprintf "https://twitter.com/%s/status/%d" t.Author.ScreenName t.Id
+            sprintf "<table id=\"%d\"><tr><td rowspan=\"2\" width=\"30\">%d</td><td rowspan=\"2\" width=\"80\"><a href=\"javascript:remove('%d')\">Remove</a><td rowspan=\"2\"><a href=\"https://twitter.com/%s\" target=\"_blank\"><img src=\"%s\"/></a></td><td><b>%s</b></td></tr><tr><td>Created : <a href=\"%s\" target=\"_blank\">%s</a> <br></td></tr></table>"
+                    id id id t.Author.ScreenName t.Author.ProfileImageUrl text tweetUrl (t.CreatedDate.ToString()))
+        |> List.fold (fun s r -> s+"&nbsp;"+r) ""
+    let html = sprintf "<html><head><script>function remove(id){return (elem=document.getElementById(id)).parentNode.removeChild(elem);}</script></head><body>%s</body></html>" rows
+    System.IO.File.WriteAllText(filename, html)
+
+let calendar = CultureInfo.CurrentCulture.Calendar
+let getGroupKey (date:DateTime) =
+    let day  = calendar.GetDayOfWeek(date) |> int
+    let dayDelta = (4 - (if day = 0 then 7 else day)) |> float
+    let week = calendar.GetWeekOfYear(date.AddDays(dayDelta), CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday)
+    (week, (if day = 0 then 7 else day), date.DayOfWeek)
+
+// Save tweets to files
+filteredTweets
+|> Seq.groupBy (fun x-> getGroupKey x.CreatedDate)
+|> Seq.iter (fun ((week,day,dw),tweets) ->
+    let fileName = sprintf "Tweets__Week%d__Day%d_%A.html" week day dw
+    printTweetsInHtml fileName (Seq.toList tweets)
+)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Press any key to exit"
+    Console.ReadKey() |> ignore
+    0
